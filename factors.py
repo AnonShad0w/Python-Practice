d423ff46ff0263d2af9c05dad02d6801f5704af7
AnonShad0w <13724916+AnonShad0w@users.noreply.github.com>
2020-10-08 09:43:39 -0700

    Create factors.py
    Calculates the factors of a number.

---
+++ factors.py
@@ -0,0 +1,9 @@
+import math
+str_num = input('What number do you want the primes?: ')
+num = int(str_num)
+factors = []
+
+for i in range(2, num):
+    if num%i == 0:
+        factors.append(i)
+print('The factors are: ' + str(factors))
